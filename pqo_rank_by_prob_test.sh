069c0f6261ac4739be78482dbbe33215440f2c65
lsh-machine <qy46@duke.edu>
2024-08-06 12:20:30 -0400

    Script for running pqo experiment

---
+++ pqo_rank_by_prob_test.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# ### Generate pqo anchors
+# # List of query IDs
+# query_ids=( 16a 17a 18a 20a 26a)
+
+# # Loop through each query ID and run the command
+# for query_id in "${query_ids[@]}"; do
+#     echo "==========Running query with ID: $query_id=========="
+#     python robustness.py --query_id=$query_id --pqo --b=0.5
+#     echo "==========Finished query with ID: $query_id=========="
+# done
+
+# ### Generate rqo sen_dim for list of querys
+# # Directory containing the SQL files
+# DIRECTORY="query/join-order-benchmark/rank-by-prob/"
+
+# # Loop through all the SQL files in the directory
+# # python robustness.py --query_id=1a --cal_sen=sobol --rqo_query='query/join-order-benchmark/rank-by-prob/1-0-b0.5.sql'      
+# for FILE in "$DIRECTORY"*.sql
+# do
+#   # Extract the base name of the file (without directory path)
+#   BASENAME=$(basename "$FILE")
+
+#   # Extract the query_id (first number in the file name)
+#   QUERY_ID=$(echo "$BASENAME" | grep -o '^[0-9]*')
+
+#   # Construct the command and execute it
+#   CMD="python robustness.py --query_id=${QUERY_ID}a --cal_sen=sobol --rqo_query='$FILE'"
+#   echo "Executing: $CMD"
+#   eval $CMD
+# done
+
+
+# ### Generate rqo results for list of querys
+# # Directory containing the SQL files
+# DIRECTORY="query/join-order-benchmark/rank-by-prob/"
+
+# # Loop through all the SQL files in the directory
+# # python robustness.py --query_id=1a --cal_sen=sobol --rqo_query='query/join-order-benchmark/rank-by-prob/1-0-b0.5.sql'      
+# for FILE in "$DIRECTORY"*.sql
+# do
+#   # Extract the base name of the file (without directory path)
+#   BASENAME=$(basename "$FILE")
+
+#   # Extract the query_id (first number in the file name)
+#   QUERY_ID=$(echo "$BASENAME" | grep -o '^[0-9]*')
+
+#   # Construct the command and execute it
+#   CMD="python robustness.py --query_id=${QUERY_ID}a --rqo_query='$FILE'"
+#   echo "Executing: $CMD"
+#   eval $CMD
+# done
+
+### Generate pqo anchors
+# List of query IDs
+query_ids=( 1a 2a 3a 4a 16a 17a 18a 20a )
+
+# Loop through each query ID and run the command
+for query_id in "${query_ids[@]}"; do
+    echo "==========Running query with ID: $query_id=========="
+    python robustness.py --query_id=$query_id --pqo --b=0.5
+    echo "==========Finished query with ID: $query_id=========="
+done
